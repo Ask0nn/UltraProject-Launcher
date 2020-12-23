4065132f8a1c033b1a0502d381e8090df68971d4
Ask0n <askonzir@gmail.com>
2020-12-23 18:40:22 +0300

    Удаление конструктора

--- UltraProject/Models/Image.cs
+++ UltraProject/Models/Image.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using PropertyChanged;
+﻿using PropertyChanged;
+using System;
 
 namespace UltraProject.Models
 {
@@ -11,12 +10,6 @@ namespace UltraProject.Models
 
         public string Path { get => GetFullPathToImage(); set => _path = value; }
         public ImageType Type { get; set; }
-
-        public Image(string path, ImageType type)
-        {
-            Path = path;
-            Type = type;
-        }
 
         private string GetFullPathToImage()
             => Type.Equals(ImageType.FILE) ? System.IO.Path.Combine(Environment.CurrentDirectory, _path) : _path; 
