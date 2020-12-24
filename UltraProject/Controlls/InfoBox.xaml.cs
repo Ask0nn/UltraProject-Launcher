53e2a9fa474ca144a06cd237ed4f54a00f622fa0
Ask0nn <askonzir@gmail.com>
2020-12-24 21:18:49 +0300

    Добавление инфо-бокса

---
+++ UltraProject/Controlls/InfoBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using MaterialDesignThemes.Wpf;
+using PropertyChanged;
+using System.Windows;
+
+namespace UltraProject.Controlls
+{
+    /// <summary>
+    /// Логика взаимодействия для InfoBox.xaml
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public partial class InfoBox : Card
+    {
+        public InfoBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Version
+        {
+            get { return (string)GetValue(VersionProperty); }
+            set { SetValue(VersionProperty, value); }
+        }
+
+        public string URL
+        {
+            get { return (string)GetValue(URLProperty); }
+            set { SetValue(URLProperty, value); }
+        }
+
+        public static readonly DependencyProperty VersionProperty =
+            DependencyProperty.Register("Version", typeof(string), typeof(InfoBox), new PropertyMetadata(0));
+
+        public static readonly DependencyProperty URLProperty =
+            DependencyProperty.Register("URL", typeof(string), typeof(InfoBox), new PropertyMetadata(0));
+    }
+}
