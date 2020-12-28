2d9c319fc7210e6ef4b75afc8043a93f362ecc16
Ask0nn <Askonzir@gmail.com>
2020-12-28 13:30:01 +0300

    Обновление окна авторизации

---
+++ UltraProject/ViewModel/AuthorizationViewModel.cs
@@ -0,0 +1,51 @@
+﻿using PropertyChanged;
+using System.Windows.Controls;
+using System.Windows.Input;
+using UltraProject.Models;
+using UltraProject.Services;
+
+namespace UltraProject.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class AuthorizationViewModel
+    {
+        #region Свойства
+        public SettingsController<ProfileSettings> Settings { get; set; }
+        #endregion
+
+        public AuthorizationViewModel()
+        {
+            Settings = new SettingsController<ProfileSettings>("user-settings.json");
+            if (Settings.Settings.UserSettings.IsSave) Auth(Settings.Settings.UserSettings.AuthToken);
+        }
+
+        #region Команды
+        public ICommand Authorize { get => new DelegateCommand(Auth); }
+        #endregion
+
+        #region Методы
+        /// <summary>
+        /// Авторизация пользователя
+        /// </summary>
+        /// <param name="obj">PasswordBox</param>
+        private void Auth(object obj)
+        {
+            if (Settings.Settings.UserSettings.IsSave)
+                Settings.SaveSettings();
+            if (obj is PasswordBox password && !string.IsNullOrEmpty(password.Password))
+            {
+                System.Windows.MessageBox.Show(password.Password);
+            }
+        }
+        /// <summary>
+        /// Авторизация сохраненного пользователя
+        /// </summary>
+        /// <param name="token">сохраненный токен</param>
+        private void Auth(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+                return;
+        }
+        #endregion
+    }
+}
---
+++ UltraProject/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PropertyChanged;
+using UltraProject.Models;
+using UltraProject.Services;
+
+namespace UltraProject.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel
+    {
+        #region Свойства
+        public SettingsController<LauncherSettings> Settings { get; set; }
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            Settings = new SettingsController<LauncherSettings>("launcher-settings.json");
+        }
+    }
+}
